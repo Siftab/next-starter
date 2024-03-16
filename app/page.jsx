4dc76e84277f5490806a94b314741435bfcc08e7
Siftab <siiffuuu@gmail.com>
2024-03-17 01:39:59 +0600

    bg color test

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <section className='text-2xl'>
-      <div className='container mx-auto  border border-red-50'>
+      <div className='container mx-auto  border border-red-50 bg-red-600'>
         <h1>this is next js </h1>
       </div>
     </section>
