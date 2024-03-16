1e80ab498b4b3160a5e7d8c5dc50c3805db5b224
Siftab <siiffuuu@gmail.com>
2024-03-17 01:33:16 +0600

    intial commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='text-2xl'>
+      <div className='container mx-auto  border border-red-50'>
+        <h1>this is next js </h1>
+      </div>
+    </section>
+  )
+}
